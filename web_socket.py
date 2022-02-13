0d0762ef1454d79afdf69bbf8fa889f1e85a5353
ElvisCamcho <camachocv@hotmail.com>
2022-02-13 07:22:12 -0500

    everything working fine

--- web_socket.py
+++ web_socket.py
@@ -1,5 +1,7 @@
 import socket
 
+from datetime import *
+
 
 # Define socket host and port
 SERVER_HOST = '0.0.0.0'
@@ -8,9 +10,14 @@ SERVER_PORT = 12003
 # Create socket
 
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+#making socket available as fast as possible #### reserving port 
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+
+# this bind socked to a specific interface
 server_socket.bind((SERVER_HOST, SERVER_PORT))
+
+# inviting connection from else where, which is listning 
 server_socket.listen(1)
 print('Listening on port: ...' ,SERVER_PORT)
 
@@ -18,15 +25,25 @@ while True:
     # Wait for client connections
     client_connection, client_address = server_socket.accept()
 
-    # Get the client request
+    # Get the client request  --- receiving data 
     request = client_connection.recv(1024).decode()
     print(request)
-    print(type(request))
 
     # Parce header to get content of html
-    headers = request.split('\n')
+    headers = request.split('\r\n')
     filename = headers[0].split()[1]
-    print(filename)
+    #print(' ')
+    #print(headers)
+    #print(filename)
+    
+    # Wrinting request to a file   ## Making logs
+    ip = headers[1].split(':')[1]
+    date=datetime.now()
+    logs="REQUESTED_IP:{}, - -[{}] {}\n".format(ip,date,headers[0])
+
+    with open("log.txt", 'a') as making_log_file:
+        making_log_file.write(logs)  
+    
     
     # Get the content of the files
     if filename == '/' or filename== '/index.html':
